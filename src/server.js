8d04c1abd38a2b5b13ed15acf586bc4993381b55
EvoFox <foulkes.joshua@live.co.uk>
2022-07-04 11:08:43 +0100

    futureproofed server for hosting

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ const express = require("express");
 // Create app object, which houses server instructions.
 const app = express();
 
+// Set _PORT equal to EITHER a port defined by an environment variable file OR set to 5001
+// This is done to allow for a server host to provide a port that we will host on, or default to 5001 if not provided.
+const _PORT = process.env.PORT || 5001;
+
 // Tell app to use the public folder when a request is made to /static.
 app.use("/static", express.static("public"));
 
 // Listening on localhost:5001 for requests to webserver
-app.listen(5001, () => {
-	console.log("Listening on port 5001");
+app.listen(_PORT, () => {
+	console.log(`Listening on port ${_PORT}`);
 });
